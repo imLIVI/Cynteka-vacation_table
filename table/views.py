e844edbf0d07c94a8c2b7055641f592dd5e3e281
Vlad G <goncharov@cynteka.ru>
2023-08-03 17:44:04 +0300

    update info in db

--- table/views.py
+++ table/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from . import db
 
-# Create your views here.
+
 def table(request):
     info_in_table = db.check_info_in_portal_table()
-    id_list = []
-    company_name_list = []
-    person_list = []
+    all_data = []
     for i in info_in_table:
-        id_list.append(i[0])
-        company_name_list.append(i[1])
-        person_list.append(i[2])
-    data = {
-        'id': id_list,
-        'company_name': company_name_list,
-        'person': person_list
-    }
-    print(data)
-    return render(request, 'tablePage.html', data)
+        id_list = i[0]
+        company_name_list = i[1]
+        person_list = i[2]
+        data = {
+            'id': id_list,
+            'company_name': company_name_list,
+            'person': person_list
+        }
+        all_data.append(data)
+        print(data)
+    # print(all_data)
+    return render(request, 'tablePage.html', all_data)
