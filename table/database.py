31f1d15758673ecd4b10261448bd96a1a92a3f5d
imLIVI <lesly120898@gmail.com>
2023-09-29 18:22:15 +0300

    Add connection to db

---
+++ table/database.py
@@ -0,0 +1,40 @@
+import psycopg2
+
+def query_execute(cursor):   
+    query = "select integration_vacation.id, company.short_name, person.last_name " +  "from integration_vacation " + "left join company on integration_vacation.company_id = company.id " + "left join person on integration_vacation.manager_id = person.id"
+    cursor.execute(query)
+
+    # Получение строк запроса
+    query_res = cursor.fetchall()
+
+    return query_res
+
+def connect():
+    # Подключение к порталу
+    connection = psycopg2.connect(
+        dbname="cyfoman", 
+        host="172.16.50.20", 
+        user="lipatnikova", 
+        password="J832s357", 
+        port="5432")
+    
+    with connection:
+        with connection.cursor() as cursor:
+            print("Connection to Zakupay is success")
+    
+    # Получаем объект для выполнения запрососв к БД
+    cursor = connection.cursor()
+        
+    # Для того, чтобы query автоматически выполнялись при вызове метода cursor.execute()
+    connection.autocommit = True
+
+    return cursor, connection
+
+
+# Закрытие подключения
+def close(cursor, connection):
+    cursor.close()
+    connection.close()
+
+
+
--- table/views.py
+++ table/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
-from . import db
+from . import database
 
 # Create your views here.
 def table(request):
-    info_in_table = db.check_info_in_portal_table()
-    id_list = []
-    company_name_list = []
-    person_list = []
-    for i in info_in_table:
-        id_list.append(i[0])
-        company_name_list.append(i[1])
-        person_list.append(i[2])
-    data = {
-        'id': id_list,
-        'company_name': company_name_list,
-        'person': person_list
-    }
-    print(data)
-    return render(request, 'tablePage.html', data)
+    cursor, connection = database.connect()
+    res_query = database.query_execute(cursor)
+    print(res_query)
+
+    database.close(cursor, connection)
+    return render(request, 'tablePage.html', {})
+
