c4f4dca9e9313604d2bfdfc5d2204e2622ad5cba
imLIVI <lesly120898@gmail.com>
2023-10-02 12:34:46 +0300

    Add the query parsing from the database and add the mapping to html

--- table/views.py
+++ table/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from . import database
 
+# Парсинг запроса для формирования списка словарей
+def query_parse(persons_list):
+    dict_list = []
+    for person_info in persons_list:
+        dict = {
+        'id': person_info[0],
+        'company_name': person_info[1],
+        'person': person_info[2]
+        }
+        dict_list.append(dict)
+    return dict_list  
+
+def get_context(dict_list):
+    return {'dict_list' : dict_list}
+
 # Create your views here.
 def table(request):
     cursor, connection = database.connect()
     res_query = database.query_execute(cursor)
-    print(res_query)
+    #query_parse(res_query)
+
+    #print(res_query)
 
     database.close(cursor, connection)
-    return render(request, 'tablePage.html', {})
+    dict_list = get_context(query_parse(res_query))
 
+    #TODO: вместо {} должен передаваться словарь: context: persons
+    return render(request, 'tablePage.html', context=dict_list)
